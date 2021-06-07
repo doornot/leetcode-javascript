c637e5f0361312dce4d7cb699a552c4472945200
wangbiao <wangb8909@gmail.com>
2021-06-07 23:25:39 +0800

    u

--- Array/交集、并集、差集.js
+++ Array/交集、并集、差集.js
@@ -3,16 +3,23 @@
  */
 
 // 交集
-intersect = (a, b) => {
+const intersect = (a, b) => {
   return a.filter((v) => b.indexOf(v) > -1)
 }
 
 // 并集
-union = (a, b) => {
+const union = (a, b) => {
   return a.concat(b.filter((v) => !(a.indexOf(v) > -1)))
 }
 
 // 差集
-minus = (a, b) => {
+const minus = (a, b) => {
   return a.filter((v) => b.indexOf(v) === -1)
 }
+
+const a = [1, 2, 3, 4, 5]
+const b = [3, 4, 5, 6, 7]
+
+console.log('intersect: ' + intersect(a, b))
+console.log('union: ' + union(a, b))
+console.log('minus: ' + minus(a, b))
